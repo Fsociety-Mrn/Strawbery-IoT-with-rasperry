fad5c81454ccb1ff59fd034751ecb88637acf175
Fsociety-Mrn <lisboamillen30@gmail.com>
2023-03-07 17:38:08 +0800

    updated

--- main.py
+++ main.py
@@ -4,50 +4,58 @@ from Firebase.firebase import firebaseReadChild,firebaseUpdateChild
 
 import os
 import time
-import Adafruit_DHT
-import threading
-import Adafruit_ADS1x15
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 # ********************************************************* Strawberry Backend ********************************************************* #
 
+# *********************** for soil moisture *********************** #
 
+# Define calibration constants
+VOLTAGE_MIN = 0.0  # minimum voltage reading from sensor
+VOLTAGE_MAX = 3.3  # maximum voltage reading from sensor
+MOISTURE_MIN = 0   # moisture value corresponding to VOLTAGE_MIN
+MOISTURE_MAX = 100 # moisture value corresponding to VOLTAGE_MAX
 
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
 
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1115(i2c)
 
-# Initialize the ADS1115 ADC (change address to match your setup)
-adc = Adafruit_ADS1x15.ADS1115(address=0x48)
-
-# Choose a gain of 1 for reading voltages from 0 to 4.096V
-GAIN = 1
-
-# Define the analog input channel for the soil moisture sensor
-moisture_channel = 0
+Moisture_1 = AnalogIn(ads, ADS.P0)
+Moisture_2 = AnalogIn(ads, ADS.P1)
 
 # ********************** setup functions ********************** # 
 def setup():
+    
+    # Create single-ended input on channel 0
+    chan = AnalogIn(ads, ADS.P0)
+    
     GPIO.setwarnings(False) 
     GPIO.setmode(GPIO.BOARD)
+    
+
 
 # ********************** functions ********************** #  
-def adc_to_voltage(adc_value):
-    # The ADS1115 has 16-bit resolution, so there are 2^16 possible values
-    # The voltage range for a gain of 1 is -4.096V to +4.096V, so each
-    # bit represents 8/65536 = 0.00012207V
-    voltage = adc_value * 0.00012207
-    return voltage
+
+def calcu_moisture(moisture_sensor):
+    voltage = moisture_sensor.voltage
+
+    # Convert the voltage to a moisture value using linear interpolation
+    moisture = (voltage - VOLTAGE_MIN) * \
+               (MOISTURE_MAX - MOISTURE_MIN) / \
+               (VOLTAGE_MAX - VOLTAGE_MIN) + MOISTURE_MIN
+    return float(moisture)
+
 
 # ********************** loop function ********************** #
 def loop():
     
-    # Read the ADC value from the soil moisture sensor
-    moisture_adc = adc.read_adc(moisture_channel, gain=GAIN)
-    # Convert the ADC reading to voltage
-    moisture_voltage = adc_to_voltage(moisture_adc)
-    # Print the moisture voltage
-    print("Moisture voltage: {:.2f}V".format(moisture_voltage))
-    # Wait for 1 second before taking another reading
-    time.sleep(1)
-    
+    print("M1: ",calcu_moisture(Moisture_1))
+    print("M2: ",calcu_moisture(Moisture_2))
     return loop()
 
 
